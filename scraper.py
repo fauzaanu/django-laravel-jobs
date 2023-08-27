8d9c96d76847f334cd0e88a177f82cbb9540e3d8
fauzaanu <fauzaangasim@gmail.com>
2023-08-28 01:10:03 +0500

    Data updated: 2023-08-28T01:10:03.910282

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ import datetime
 import json
 import os
 import subprocess
-from datetime import datetime
 
 def main():
   new_data = {'date': datetime.datetime.now().isoformat(), 'jobs': {'DJANGO_URL': '', 'LARAVEL_URL': ''}}
@@ -19,30 +18,26 @@ def main():
         job_count = int(match.group(1).replace(',', '')) if match else 0
         new_data['jobs'][key] = job_count
         browser.close()
-  with open('data.json', 'r+') as f:
-    if os.stat('data.json').st_size != 0:
+  with open('public/data.json', 'r+') as f:
+    if os.stat('public/data.json').st_size != 0:
         data = json.load(f)
         data.append(new_data)
     else:
         data = [new_data]
     f.seek(0)
     json.dump(data, f)
-    
-
 
 def commit():
     # Add all changes to the Git staging area
     subprocess.run(["git", "add", "."], check=True)
 
     # Commit the changes with a message containing the current timestamp
-    commit_message = f"Data updated: {datetime.now().isoformat()}"
+    commit_message = f"Data updated: {datetime.datetime.now().isoformat()}"
     subprocess.run(["git", "commit", "-m", commit_message], check=True)
 
     # Push the changes to the remote repository
     subprocess.run(["git", "push"], check=True)
 
-    
-
 if __name__ == '__main__':
   main()
-  commit("commit by scraper on " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + " UTC")
+  commit()
