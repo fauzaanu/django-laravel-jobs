0ff28084ee901ac41f44d119bb5a02aaf65c5732
fauzaanu <fauzaangasim@gmail.com>
2023-08-28 11:49:42 +0500

    Data updated: 2023-08-28T11:49:42.506573

--- public/script.js
+++ public/script.js
@@ -4,18 +4,24 @@ fetch("data.json")
     let tbody = document.querySelector("#data tbody");
     data.forEach((item) => {
       let tr = document.createElement("tr");
+      let djangoImg = item.imgs.DJANGO_URL
+        ? `<img src="public/imgs/${item.imgs.DJANGO_URL}.png" width="100" />`
+        : "";
+      let laravelImg = item.imgs.LARAVEL_URL
+        ? `<img src="public/imgs/${item.imgs.LARAVEL_URL}.png" width="100" />`
+        : "";
       tr.innerHTML = `
                 <td class="border px-4 py-2">${item.date}</td>
                 <td class="border px-4 py-2">
                   <a href="public/imgs/${item.imgs.DJANGO_URL}.png" target="_blank">
                     ${item.jobs.DJANGO_URL}
-                    <img src="public/imgs/${item.imgs.DJANGO_URL}.png" width="100" />
+                    ${djangoImg}
                   </a>
                 </td>
                 <td class="border px-4 py-2">
                   <a href="public/imgs/${item.imgs.LARAVEL_URL}.png" target="_blank">
                     ${item.jobs.LARAVEL_URL}
-                    <img src="public/imgs/${item.imgs.LARAVEL_URL}.png" width="100" />
+                    ${laravelImg}
                   </a>
                 </td>
             `;
