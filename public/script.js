3eaff31d58fcfc47a06ec3a976e8b69200181cd0
fauzaanu <fauzaangasim@gmail.com>
2023-08-28 12:14:02 +0500

    Data updated: 2023-08-28T12:14:02.174118

--- public/script.js
+++ public/script.js
@@ -4,22 +4,28 @@ fetch("data.json")
     let tbody = document.querySelector("#data tbody");
     data.forEach((item) => {
       let tr = document.createElement("tr");
-      let djangoImg = item.imgs.DJANGO_URL
-        ? `<img src="public/imgs/${item.imgs.DJANGO_URL}.png" width="100" />`
-        : "";
-      let laravelImg = item.imgs.LARAVEL_URL
-        ? `<img src="public/imgs/${item.imgs.LARAVEL_URL}.png" width="100" />`
-        : "";
+      let djangoImg =
+        item.imgs && item.imgs.DJANGO_URL
+          ? `<img src="imgs/${item.imgs.DJANGO_URL}.png" class="w-full" />`
+          : "";
+      let laravelImg =
+        item.imgs && item.imgs.LARAVEL_URL
+          ? `<img src="imgs/${item.imgs.LARAVEL_URL}.png" class="w-full" />`
+          : "";
       tr.innerHTML = `
-                <td class="border px-4 py-2">${item.date}</td>
+                <td class="border px-4 py-2 text-xs">${item.date}</td>
                 <td class="border px-4 py-2">
-                  <a href="public/imgs/${item.imgs.DJANGO_URL}.png" target="_blank">
+                  <a href="imgs/${
+                    item.imgs && item.imgs.DJANGO_URL
+                  }.png" target="_blank">
                     ${item.jobs.DJANGO_URL}
                     ${djangoImg}
                   </a>
                 </td>
                 <td class="border px-4 py-2">
-                  <a href="public/imgs/${item.imgs.LARAVEL_URL}.png" target="_blank">
+                  <a href="imgs/${
+                    item.imgs && item.imgs.LARAVEL_URL
+                  }.png" target="_blank">
                     ${item.jobs.LARAVEL_URL}
                     ${laravelImg}
                   </a>
