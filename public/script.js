44a8b3f30e37dbc287096162bc857d5a9ee32308
fauzaanu <fauzaangasim@gmail.com>
2023-08-28 13:51:56 +0500

    Data updated: 2023-08-28T13:51:56.461230

--- public/script.js
+++ public/script.js
@@ -2,10 +2,7 @@ fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
     let tbody = document.querySelector("#data tbody");
-    let grid = document.querySelector("#grid");
-
     data.forEach((item) => {
-      // Process data for table
       let tr = document.createElement("tr");
       let djangoImg =
         item.imgs && item.imgs.DJANGO_URL
@@ -35,13 +32,23 @@ fetch("data.json")
                 </td>
             `;
       tbody.appendChild(tr);
+    });
+  });
 
-      // Process data for grid
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let grid = document.querySelector("#grid");
+    console.log("Grid:", grid); // Check if grid is selected correctly
+    console.log("Data:", data); // Check the data
+    data.forEach((item) => {
       let div = document.createElement("div");
+      // If 'DJANGO_URL' value is higher, assign bg-green-800, otherwise, assign bg-red-500
       div.className =
         item.jobs.DJANGO_URL >= item.jobs.LARAVEL_URL
           ? "w-4 h-4 bg-green-800"
           : "w-4 h-4 bg-red-500";
+      console.log("Div:", div); // Check each div
       grid.appendChild(div);
     });
   });
